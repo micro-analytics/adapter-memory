dec3bcdccb49fb3b23060d4e511bb9d25d86219b
Rolf Erik Lekang <me@rolflekang.com>
2017-05-17 18:35:44 +0200

    feat: Add filtering in get and getAll

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const Observable = require("zen-observable");
 
 let data = {};
-
 let handlers = [];
+
+const escapeRegexp = require('escape-regex')
+
+const keyRegex = (str) => {
+  str = str.split('*').map( s => escapeRegexp(s)).join('*')
+  return new RegExp('^' + str.replace('*','.*'))
+}
 
 const observable = new Observable(observer => {
   handlers.push(data => observer.next(data));
@@ -12,28 +18,51 @@ const observable = new Observable(observer => {
   };
 });
 
-function get(key) {
-  return Promise.resolve(data[key]);
+function get(key, options) {
+  const value = data[key] || { views: [] };
+  return Promise.resolve({
+    views: value.views.filter(view => {
+      if (options && options.before && view.time > options.before) return false;
+      if (options && options.after && view.time < options.after) return false;
+      return true;
+    })
+  });
+}
+
+function keys() {
+  return Promise.resolve(Object.keys(data));
 }
 
 function put(key, value) {
   data[key] = value;
   handlers.forEach(handler => {
-    handler({key, value});
-  })
+    handler({ key, value });
+  });
+
   return Promise.resolve();
 }
 
-function getAll() {
-  return Promise.resolve(data);
+async function getAll(options) {
+  const value = {};
+  const _keys = (await keys()).filter(
+    key =>
+      options.ignoreWildcard
+        ? key.startsWith(options.pathname)
+        : key.match(keyRegex(options.pathname))
+  );
+
+  for (let key of _keys) {
+    value[key] = await get(key, {
+      before: options.before,
+      after: options.after
+    });
+  }
+
+  return Promise.resolve(value);
 }
 
 function has(key) {
   return Promise.resolve({}.hasOwnProperty.call(data, key));
-}
-
-function keys() {
-  return Promise.resolve(Object.keys(data));
 }
 
 function clear() {
