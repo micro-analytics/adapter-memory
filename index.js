d1e6e781bec3f2e2f00a1651260b41ddb7090f12
Rolf Erik Lekang <me@rolflekang.com>
2017-05-17 18:35:12 +0200

    feat: Add support for subscribe
    Fixes #1

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const Observable = require("zen-observable");
+
 let data = {};
+
+let handlers = [];
+
+const observable = new Observable(observer => {
+  handlers.push(data => observer.next(data));
+  let index = handlers.length;
+  return () => {
+    handlers = [...handlers.slice(0, index), ...handlers.slice(index)];
+  };
+});
 
 function get(key) {
   return Promise.resolve(data[key]);
@@ -6,11 +18,14 @@ function get(key) {
 
 function put(key, value) {
   data[key] = value;
+  handlers.forEach(handler => {
+    handler({key, value});
+  })
   return Promise.resolve();
 }
 
-function getAll(options) {
-  return data;
+function getAll() {
+  return Promise.resolve(data);
 }
 
 function has(key) {
@@ -18,11 +33,15 @@ function has(key) {
 }
 
 function keys() {
-  return Object.keys(data);
+  return Promise.resolve(Object.keys(data));
 }
 
 function clear() {
   data = {};
 }
 
-module.exports = { get: get, put: put, has: has, keys: keys, getAll: getAll, clear: clear };
+function subscribe(listener) {
+  return observable.subscribe(listener);
+}
+
+module.exports = { get, put, getAll, has, keys, clear, subscribe };
